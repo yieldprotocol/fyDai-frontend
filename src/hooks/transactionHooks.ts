b2502984f393e2dc39136476570c7ef1c083a844
brucedonovan <bruce@5swim.com>
2020-06-29 22:23:58 +0100

    feature: documentaion transaction hooks

--- src/hooks/transactionHooks.ts
+++ src/hooks/transactionHooks.ts
@@ -31,12 +31,29 @@ const contractMap = new Map<string, any>([
   ['EthProxy', EthProxy.abi],
 ]);
 
-// SendTx is a generic function to interact with any contract, primarily used for development/testing.
+/**
+ * SendTx is a generic function to interact with any contract.
+ * Primarily used for development/testing,
+ * or for once off interactions with a contract.
+ * Currently, There are no notifications other than console logs.
+ * @returns { function } sendTx
+ * @returns { boolean } sendTxActive
+ */
+// 
 export const useSendTx = () => {
   const { state: { signer, account } } = React.useContext(ConnectionContext);
   // const { library, account } = useWeb3React();
   // const signer = library.getSigner();
   const [ sendTxActive, setSendTxActive ] = React.useState<boolean>();
+  
+  /**
+   * Sends a transaction
+   * @param {string} contractAddress address of the contract to send to.
+   * @param {string} contractName name of the contract to call (uses this to get the abi from a contract map)
+   * @param {string} fn name of the function to call 
+   * @param {any[]} data array of any arguments required by the contract function 
+   * @param {BigNumber} value if the tx is to a payable contract, use a bigNumber value here. 
+   */
   const sendTx = async (contractAddr:string, contractName:string, fn:string, data:any[], value:ethers.BigNumber ) => {
     let tx;
     console.log(contractAddr, contractMap.get(contractName), signer); 
@@ -57,10 +74,26 @@ export const useSendTx = () => {
   return [ sendTx, sendTxActive ] as const;
 };
 
+/**
+ * Hook for making blockchain calls
+ * Does not incur gas charges
+ * But only applicable if contract function has a view modifier
+ * Fails ( mostly silently ) on functions that require gas.
+ * @returns { function } callTx
+ * @returns { boolean } callTxActive
+ */
 export const useCallTx = () => {
+
   const { state: { provider, altProvider } } = React.useContext(ConnectionContext);
   // const { library: provider, account } = useWeb3React();
   const [ callTxActive, setCallTxActive ] = React.useState<boolean>();
+  /**
+   * Get data from the blockchain via provider (no signer reqd)
+   * @param {string} contractAddress address of the contract to be called
+   * @param {string} contractName name of the contract to call (uses this to get the abi from a contract map)
+   * @param {string} fn name of the function to call 
+   * @param {any[]} data array of any arguments required by the contract function 
+   */
   const callTx = async (
     contractAddr:string,
     contractName:string,
@@ -76,15 +109,32 @@ export const useCallTx = () => {
   return [ callTx, callTxActive ] as const;
 };
 
+/**
+ * Hook for getting native balances and token balances
+ * @returns { function } getEthBalance
+ * @returns { boolean } getTokenBalance
+ */
 export function useBalances() {
   const { state: { provider, account } } = React.useContext(ConnectionContext);
   // const { library: txProvider, account } = useWeb3React();
+
+  /**
+   * Simple account balance call (WEI)
+   * @returns balance in Wei! 
+   */
   const getEthBalance = async () => {
     if (!!provider && !!account) {
       const balance = await provider.getBalance(account);
       return balance;
     } return ethers.BigNumber.from('0');
   };
+
+  /**
+   * Get data from the blockchain via provider (no signer reqd)
+   * @param {string} tokenAddr address of the Token
+   * @param {string} abi abi of the token (probably ERC20 in this case)
+   * @returns whatever token value
+   */
   const getTokenBalance = async (tokenAddr:string, abi:string) => {
     if (!!provider && !!account) {
       const contract = new ethers.Contract(tokenAddr, contractMap.get(abi), provider);
