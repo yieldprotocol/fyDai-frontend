4b4257b5a5bac9e460698128c720e8a90a40a2ba
Rishabh Varshney <it.is.rishabh@gmail.com>
2020-08-05 07:34:38 -0400

    changed footer icons, fixed spacing and optimized for mobile

--- src/components/YieldFooter.tsx
+++ src/components/YieldFooter.tsx
@@ -1,61 +1,94 @@
 import React from 'react';
 import { useWeb3React } from '@web3-react/core';
 
-import { 
-  Anchor, 
-  Grommet, 
-  base, 
-  Tabs, 
-  Tab, 
-  Grid,
-  Main,
-  Footer,
-  Button,
-  Box,
-  Text 
-} from 'grommet';
-import { 
-  FaSun as Sun,
-  FaMoon as Moon,
-  FaGithub as Github,
-  FaInfo as Info,
-  FaFileAlt as Docs,
-  FaVial as Test,
-} from 'react-icons/fa';
+import { Anchor, Footer, Button, Box } from 'grommet';
+
+import {
+  FiSun as Sun,
+  FiMoon as Moon,
+  FiGithub as Github,
+  FiInfo as Info,
+  FiFileText as Docs,
+  FiCodesandbox as Test,
+} from 'react-icons/fi';
 
 // TODO: use theming context properly - no cheating :)
 import { yieldTheme } from '../themes';
 import { useSignerAccount } from '../hooks';
 
 // import { ConnectionContext } from '../contexts/ConnectionContext';
-  
+
 const YieldFooter = (props: any) => {
-
-
-  const { setShowTestLayer, showTestLayer, setDarkmode, darkmode, changeConnection } = props;
+  const {
+    setShowTestLayer,
+    showTestLayer,
+    setDarkmode,
+    darkmode,
+    changeConnection,
+  } = props;
   const { account } = useSignerAccount();
+
+  const IconSize = '1.15rem';
+  const IconGap = 'small';
 
   return (
     <Footer
-      gap="xlarge"
-      fill='horizontal'
-      pad={{ horizontal: 'large', vertical:'medium' }}
+      align="center"
+      fill="horizontal"
+      pad={{ horizontal: 'none', vertical: 'medium' }}
     >
-      <Box direction='row' gap='small'>
-        <Anchor color='grey'><Github /></Anchor>
-        <Anchor color='grey'><Docs /></Anchor>
-        <Anchor color='grey'><Info /></Anchor>
+      <Box
+        direction="row"
+        gap={IconGap}
+        style={{
+          position: 'relative',
+          top: '2px',
+        }}
+      >
+        <Anchor color="grey">
+          <Github size={IconSize} />
+        </Anchor>
+        <Anchor color="grey">
+          <Docs size={IconSize} />
+        </Anchor>
+        <Anchor color="grey">
+          <Info size={IconSize} />
+        </Anchor>
       </Box>
       <Box>
-        { !account && <Button style={{ minWidth:'160px' }} label='Connect to a wallet' onClick={()=>changeConnection()} />}
+        {!account && (
+          <Button
+            label="Connect wallet"
+            onClick={() => changeConnection()}
+            style={{
+              fontWeight: 600,
+            }}
+          />
+        )}
       </Box>
-      <Box direction='row' gap='medium'>
-        <Test onClick={()=>setShowTestLayer(!showTestLayer)} color={showTestLayer?yieldTheme.global.colors.brand.light:'grey'} /> 
-        <Box direction='row'>
-          { darkmode?
-            <Sun onClick={()=>setDarkmode(!darkmode)} color={yieldTheme.global.colors.brand.light} />
-            :
-            <Moon onClick={()=>setDarkmode(!darkmode)} />}
+      <Box
+        direction="row"
+        gap={IconGap}
+        style={{
+          position: 'relative',
+          top: '2px',
+        }}
+      >
+        <Test
+          onClick={() => setShowTestLayer(!showTestLayer)}
+          size={IconSize}
+          color={showTestLayer ? yieldTheme.global.colors.brand.light : 'grey'}
+        />
+        <Box direction="row">
+          {darkmode ? (
+            <Sun
+              onClick={() => setDarkmode(!darkmode)}
+              size={IconSize}
+              color={yieldTheme.global.colors.brand.light}
+            />
+          ) : (
+            <Moon onClick={() => setDarkmode(!darkmode)} size={IconSize} />
+          )}
         </Box>
       </Box>
     </Footer>
