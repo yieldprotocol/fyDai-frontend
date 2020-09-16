580d94e5ce61ffd6ed69acee97666bcf2bef333a
Bruce Donovan <brucedonovan@Bruces-MBP.lan>
2020-09-16 15:12:24 +0100

    refactor: yield Context remove redundancy

--- src/contexts/YieldContext.tsx
+++ src/contexts/YieldContext.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useReducer, useEffect, useContext, createContext } from 'react';
 import { ethers } from 'ethers';
 import moment from 'moment';
-
-import { useWeb3React } from '@web3-react/core';
 import * as utils from '../utils';
 import { NotifyContext } from './NotifyContext';
 
@@ -10,13 +8,14 @@ import { useCachedState, } from '../hooks/appHooks';
 import { useCallTx } from '../hooks/chainHooks';
 import { useEvents } from '../hooks/eventHooks';
 import { useSignerAccount } from '../hooks/connectionHooks';
-
-// TODO: fix these cyclic errors
 import { useMigrations } from '../hooks/migrationHook';
 
-const YieldContext = React.createContext<any>({});
+import { IYieldSeries } from '../types';
+
+const YieldContext = createContext<any>({});
 
 const eDaiList = ['eDai0', 'eDai1', 'eDai2', 'eDai3', 'eDai4'];
+const eDaiDateList = ['20Oct', '20Sep', '21Apr', '21Jan', '21Jul'];
 const seriesColors = ['#726a95', '#709fb0', '#a0c1b8', '#f4ebc1', '#3f51b5', '#5677fc', '#03a9f4'];
 const contractList = [
   'Controller',
@@ -26,7 +25,6 @@ const contractList = [
   'Vat',
   'Weth',
   'YieldProxy',
-  'Liquidations',
 ];
 
 // reducer
@@ -76,10 +74,8 @@ const initState = {
 };
 
 const YieldProvider = ({ children }: any) => {
-  const [state, dispatch] = React.useReducer(reducer, initState);
+  const [state, dispatch] = useReducer(reducer, initState);
   const { provider, fallbackProvider } = useSignerAccount();
-  const { chainId } = useWeb3React('fallback');
-
   const { dispatch: notifyDispatch } = useContext(NotifyContext);
 
   /* cache|localStorage declarations */
@@ -94,7 +90,6 @@ const YieldProvider = ({ children }: any) => {
 
   /**
    * @dev internal fn: Get all public Yield addresses from localStorage (or chain if no cache)
-   *
    * */
   const _getProtocolAddrs = async (
     forceUpdate: boolean
@@ -103,33 +98,36 @@ const YieldProvider = ({ children }: any) => {
     let _deployedContracts: any;
   
     try {
+
+      /* Load yield core contract addresses */
       if ( !cachedContracts || forceUpdate) {
         const contractAddrs = await getAddresses(contractList);
         _deployedContracts = Object.fromEntries(contractAddrs);
         window.localStorage.removeItem('deployedContracts');
         setCachedContracts(_deployedContracts);
         console.log('Contract addresses updated:', _deployedContracts);
-
       } else {
         _deployedContracts = cachedContracts;
       }
 
+      /* Load series specific contract addrs */
       if (!cachedSeries || (cachedSeries.length !== eDaiList.length) || forceUpdate) {
-        // TODO: better implementation of iterating through series (possibly a list length from contracts function?)
         const _list = await getAddresses(eDaiList);
+        // const _list = await getAddresses(eDaiDateList.map((x:any)=> `eDai${x}`));   
+        // const _poolList = await getAddresses(eDaiDateList.map((x:any)=> `eDaiLP${x}`));
         const _seriesList = Array.from(_list.values());
 
         await Promise.all(
           _seriesList.map(async (x: string, i: number) => {
-            const name = await callTx(x, 'EDai', 'name', []);
+            const symbol = await callTx(x, 'EDai', 'symbol', []);
             const maturity = await callTx(x, 'EDai', 'maturity', []);
-            // const _peripheralAddrs = await getAddresses([ `${name}-Pool` ]);
-            // const poolAddress = _peripheralAddrs.get(`${name}-Pool`);
+
             const _peripheralAddrs = await getAddresses([ `eDai${i}-Pool` ]);
             const poolAddress = _peripheralAddrs.get(`eDai${i}-Pool`);
+            // const poolAddress_ = _poolList.get(symbol.splice(4, 0, 'LP'));                    
             return {
               eDaiAddress: x,
-              name,
+              symbol,
               maturity: maturity.toNumber(),
               poolAddress,
               maturity_: new Date(maturity * 1000),
@@ -138,10 +136,9 @@ const YieldProvider = ({ children }: any) => {
             };
           })
         ).then((res: any) => _deployedSeries.push(...res));
-
         window.localStorage.removeItem('deployedSeries');
         setCachedSeries(_deployedSeries);
-        console.log('Series contract addresses updated');
+        console.log('Series addresses updated');
       } else {
         _deployedSeries.push(...cachedSeries);
       }
@@ -150,7 +147,7 @@ const YieldProvider = ({ children }: any) => {
       // loadRetried && 
       notifyDispatch({
         type: 'fatal',
-        payload: { message: 'Error finding Yield Protocol addresses: Please check you are on a supported network.' },
+        payload: { message: 'Error loading Yield Protocol addresses: Please check you are on a supported network.' },
       });
       console.log(e);
     }
@@ -159,8 +156,8 @@ const YieldProvider = ({ children }: any) => {
 
   /* Get feed data from cache first (for offline support) */
   const _getFeedData = async (
-    deployedContracts: any,
-    deployedSeries: any
+    _deployedContracts: any,
+    _deployedSeries: IYieldSeries[]
   ): Promise<any> => {
     let _state: any = {};
     /* For for initial loading and offline support */
@@ -169,7 +166,7 @@ const YieldProvider = ({ children }: any) => {
     } else {
       _state = state.feedData;
     }
-    const _ilks = await callTx(deployedContracts.Vat, 'Vat', 'ilks', [ethers.utils.formatBytes32String('ETH-A') ]);
+    const _ilks = await callTx(_deployedContracts.Vat, 'Vat', 'ilks', [ethers.utils.formatBytes32String('ETH-A') ]);
     /* parse and return feed data if reqd. */
     const _feedData = {
       ..._state,
@@ -186,7 +183,7 @@ const YieldProvider = ({ children }: any) => {
   /**
    * @dev get PUBLIC, non-cached, non-user specific yield protocol general data
    */
-  const _getYieldData = async (deployedContracts: any): Promise<any> => {
+  const _getYieldData = async (_deployedContracts: any, _deployedSeries: IYieldSeries[]): Promise<any> => {
     const _yieldData: any = {};
     // _yieldData.ethPosted = await collateralPosted(deployedContracts.Controller, 'ETH-A');
     return {
@@ -194,7 +191,7 @@ const YieldProvider = ({ children }: any) => {
     };
   };
 
-  const _addListeners = async (_deployedContracts: any) => {
+  const _addListeners = async (_deployedContracts: any, _deployedSeries: IYieldSeries[] ) => {
     // Add Maker rate/spot changes
     provider &&
       addEventListener(
@@ -204,13 +201,12 @@ const YieldProvider = ({ children }: any) => {
         [],
         (x: any) => {
           console.log('MAKER listener', x);
-          // dispatch({ type:'updateFeedData', payload: {...feedData, feedData.ilks })
+          // dispatch({ type:'updateFeedData', payload: {...feedData, x.ilks })
         }
       );
-    // TODO: add event listener for AMM
   };
 
-  const initContext = async () => {
+  const init = async () => {
     
     /* Init start */
     dispatch({ type: 'isLoading', payload: true });
@@ -223,16 +219,16 @@ const YieldProvider = ({ children }: any) => {
     /* 2. Fetch feed/stream data (from cache initially if available) and init event listeners */
     dispatch({
       type: 'updateFeedData',
-      payload: await _getFeedData(deployedContracts, deployedSeries),
+      payload: await _getFeedData(deployedContracts, deployedSeries ),
     });
 
     // 2.1 Add event listeners
-    _addListeners(deployedContracts);
+    _addListeners(deployedContracts, deployedSeries);
 
     /* 3. Fetch auxilliary (PUBLIC non-cached, non-user specific) yield and series data */
     dispatch({
       type: 'updateYieldData',
-      payload: await _getYieldData(deployedContracts),
+      payload: await _getYieldData(deployedContracts, deployedSeries),
     });
 
     /* Init end */
@@ -241,8 +237,7 @@ const YieldProvider = ({ children }: any) => {
 
   /* Init app and re-init app on change of fallback provider network  */
   useEffect(() => {
-    // provider && (async () => initContext() )();
-    fallbackProvider && (async () => initContext() )();
+    fallbackProvider && (async () => init())();
   }, [ fallbackProvider ]);
 
   const actions = {
