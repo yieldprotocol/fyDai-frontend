5aba30bd4ee57fd80acac5e6dda73e91b31f51ee
Bruce Donovan <brucedonovan@Bruces-MBP.lan>
2020-09-30 23:19:18 +0100

    bugfix: etherscan button

--- src/components/AccountButton.tsx
+++ src/components/AccountButton.tsx
@@ -83,115 +83,110 @@ const AccountButton = (props: any) => {
         align={{ top: 'bottom' }}
         plain
       >
-
         {lastCompletedTx &&
         <Box
           pad='small'
           background='#f0f0f0'
         >
-
           { abbreviateAddr(lastCompletedTx.transactionHash) }
           {lastCompletedTx.status}
-          <EtherscanButton tx={lastCompletedTx.transactionHash} />
+          <EtherscanButton txHash={lastCompletedTx.transactionHash} />
         </Box>}
-
       </Drop>);
 
   };
   
   return (
-    <Box> 
-      <Box
-        round
-        direction='row'
-        align='center'
-        background={account?'#f0f0f0':undefined}
-        onMouseOver={() => setOver(true)}
-        onMouseLeave={() => setOver(false)}
-      > 
-        { pendingTxs.length===0 && !txCompleteOpen &&
-          <Box pad={{ left:'small', right:'large' }} direction='row' gap='small' align='center'>
-            <Text size='xsmall'><DaiMark /></Text>
-            <Loading condition={!position.daiBalance} size='xsmall'>
-              <Text size='xsmall' textAlign='center'>{position?.daiBalance_} </Text>
-            </Loading>
-          </Box>}
+    <Box
+      round
+      direction='row'
+      align='center'
+      background={account?'#f0f0f0':undefined}
+      onMouseOver={() => setOver(true)}
+      onMouseLeave={() => setOver(false)}
+    > 
+      { pendingTxs.length===0 && !txCompleteOpen &&
+      <Box pad={{ left:'small', right:'large' }} direction='row' gap='small' align='center'>
+        <Text size='xsmall'><DaiMark /></Text>
+        <Loading condition={!position.daiBalance} size='xsmall'>
+          <Text size='xsmall' textAlign='center'>{position?.daiBalance_} </Text>
+        </Loading>
+      </Box>}
         
-        {pendingTxs.length>0 &&  
-          <Box
-            ref={pendingRef}
-            direction='row'
-            margin={{ right:'-20px' }}
-            pad={{ vertical: 'xsmall', left:'small', right:'25px' }}
-            round
-            animation='slideLeft'
-          >
-            <Text size='small'> Transaction pending ... </Text>
-          </Box>}
-
+      {pendingTxs.length>0 &&  
+      <Box
+        ref={pendingRef}
+        direction='row'
+        margin={{ right:'-20px' }}
+        pad={{ vertical: 'xsmall', left:'small', right:'25px' }}
+        round
+        animation='slideLeft'
+      >
+        <Text size='small'> Transaction pending ... </Text>
+        
         { pendingRef.current && over && 
-        (       
-          <Drop
-            plain 
-            target={pendingRef.current}
-            align={{ top: 'bottom' }}
-          >
+        <Drop
+          plain 
+          target={pendingRef.current}
+          align={{ top: 'bottom' }}
+        >
             {pendingTxs.length>0 && 
               <Box     
                 pad='small'
                 background='#f0f0f0'
               >
                 {pendingTxs[0].tx.hash}
-                <EtherscanButton tx={pendingTxs[0].tx.hash} />
+                <EtherscanButton txHash={pendingTxs[0].tx.hash} />
               </Box>}
-          </Drop>)}
+        </Drop>}
+      </Box>}
 
-        { txCompleteOpen && 
-          <>    
-            <Box
-              ref={completeRef}
-              direction='row'
-              margin={{ right:'-20px' }}
-              pad={{ vertical: 'xsmall', left:'small', right:'25px' }}
-              round
-              animation='slideLeft'
-              // background='#519872'
-            >
-              {lastCompletedTx?.status === 1 &&
-                <Text color='green' textAlign='center' size='small'>              
-                  <Check /> Transaction Complete
-                </Text>}
-              {lastCompletedTx?.status !== 1 &&
-                <Text color='red' textAlign='center' size='small'>              
-                  Transaction failed
-                </Text>}
-            </Box> 
-            { completeRef.current && over && <DropBox />}
+
+
+      { txCompleteOpen && 
+      <>    
+        <Box
+          ref={completeRef}
+          direction='row'
+          margin={{ right:'-20px' }}
+          pad={{ vertical: 'xsmall', left:'small', right:'25px' }}
+          round
+          animation='slideLeft'
+        >
+          {lastCompletedTx?.status === 1 &&
+          <Text color='green' textAlign='center' size='small'>              
+            <Check /> Transaction Complete
+          </Text>}
+          {lastCompletedTx?.status !== 1 &&
+          <Text color='red' textAlign='center' size='small'>              
+            Transaction failed
+          </Text>}
+        </Box> 
+        { completeRef.current && over && <DropBox />}
                 
-          </>}
+      </>}
 
-        { account ?
-          <FlatButton
-            selected
-            background='#f0f0f0'
-            onClick={()=>openConnectLayer('ACCOUNT')}
-            label={
-              <Box gap='small' direction='row' align='center'>
-                <Text size='small'>
-                  { abbreviateAddr(account) }
-                </Text>
-                <Gear />
-              </Box>
+      { account ?
+        <FlatButton
+          selected
+          background='#f0f0f0'
+          onClick={()=>openConnectLayer('ACCOUNT')}
+          label={
+            <Box gap='small' direction='row' align='center'>
+              <Text size='small'>
+                { abbreviateAddr(account) }
+              </Text>
+              <Gear />
+            </Box>
             }
-          /> 
-          : 
-          <FlatButton 
-            onClick={() => {
-              openConnectLayer('CONNECT');
-            }}
-            label={<Text size='small'>Connect a wallet</Text>}
-          />}
-      </Box>
+        /> 
+        : 
+        <FlatButton 
+          onClick={() => {
+            openConnectLayer('CONNECT');
+          }}
+          label={<Text size='small'>Connect a wallet</Text>}
+        />}
     </Box>
   );
     
