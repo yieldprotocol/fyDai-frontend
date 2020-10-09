7844d322b88943ba25fad452c90a291d679dae5f
Bruce Donovan <brucedonovan@Bruces-MacBook-Pro.local>
2020-10-09 11:11:03 +0100

    refactor: views

--- src/views/BorrowView.tsx
+++ src/views/BorrowView.tsx
@@ -1,22 +1,10 @@
 import React, { useState  } from 'react';
-import { Box, Collapsible, Stack } from 'grommet';
-
-
-
+import { Box } from 'grommet';
 
 import RaisedBox from '../components/RaisedBox';
-import BorrowSelector from '../components/BorrowSelector';
-
 import Deposit from '../containers/Deposit';
 import Borrow from '../containers/Borrow';
 import Repay from '../containers/Repay';
-
-import CollateralSummary from '../components/CollateralSummary';
-import FlatButton from '../components/FlatButton';
-
-// const Deposit = lazy(() => import('../containers/Deposit'));
-// const Borrow = lazy(() => import('../containers/Borrow'));
-// const Repay = lazy(() => import('../containers/Repay'));
 
 interface BorrowProps {
   openConnectLayer:any;
@@ -46,11 +34,6 @@ const BorrowView = ({activeView:activeViewFromProps, openConnectLayer }: BorrowP
           background="background-front"
           round='small'
         > 
-          {/* <ReactCardFlip isFlipped={activeView === 0}>
-            <Borrow setActiveView={setActiveView} />
-            <Deposit setActiveView={setActiveView} />
-          </ReactCardFlip> */}
-
           {activeView === 0 && <Deposit setActiveView={setActiveView} openConnectLayer={openConnectLayer} /> }  
           {activeView === 1 && <Borrow setActiveView={setActiveView} openConnectLayer={openConnectLayer} /> }
           {activeView === 2 && <Repay setActiveView={setActiveView} />}
--- src/views/LendView.tsx
+++ src/views/LendView.tsx
@@ -1,23 +1,12 @@
-import React, { useContext } from 'react';
-import { Box } from 'grommet';
-import { SeriesContext } from '../contexts/SeriesContext';
-
+import React from 'react';
 import Lend from '../containers/Lend';
-import Redeem from '../containers/Redeem';
 import RaisedBox from '../components/RaisedBox';
-
-// const Lend = lazy(() => import('../containers/Lend'));
-// const Redeem = lazy(() => import('../containers/Redeem'));
 
 interface LendProps {
   openConnectLayer:any;
-  activeView?: string;
 }
 
-const LendView = ({ openConnectLayer, activeView }:LendProps) => {
-
-  const { state: seriesState } = useContext(SeriesContext);
-  const { activeSeries } = seriesState;
+const LendView = ({ openConnectLayer }:LendProps) => {
   return (
     <>
       <RaisedBox> 
@@ -25,10 +14,6 @@ const LendView = ({ openConnectLayer, activeView }:LendProps) => {
       </RaisedBox>
     </>
   );
-};
-
-LendView.defaultProps = {
-  activeView: 'lend',
 };
 
 export default LendView;
