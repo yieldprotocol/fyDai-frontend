cdd566f0bf6b7cda433dce533fd76a599592982a
Bruce Donovan <brucedonovan@Bruces-MacBook-Pro.local>
2020-10-12 09:36:13 +0100

    placeholder commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,172 +1,32 @@
-import React, { useRef, useEffect, useState, useContext, Suspense } from 'react';
-import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
-
-import { Grommet, base, Grid, Main, Box, ResponsiveContext, Nav, Layer, Collapsible } from 'grommet';
+import React, { useState, useContext, Suspense } from 'react';
+import { Text, Image, Grommet, base, Main, Box, ResponsiveContext } from 'grommet';
 import { deepMerge } from 'grommet/utils';
 import { yieldTheme } from './themes';
-
-import { modColor } from './utils';
-
-import { SeriesContext } from './contexts/SeriesContext';
-import { YieldContext } from './contexts/YieldContext';
-
-import Borrow from './containers/Borrow';
-import Lend from './containers/Lend';
-import Pool from './containers/Pool';
-import Deposit from './containers/Deposit';
-
-import YieldHeader from './components/YieldHeader';
-import YieldFooter from './components/YieldFooter';
-import Authorization from './components/Authorization';
+import logoLight from './assets/images/logo_light.svg';
 import ErrorBoundary from './components/ErrorBoundry';
 
-import YieldMark from './components/logos/YieldMark';
-
-import ConnectLayer from './containers/layers/ConnectLayer';
-import NotifyLayer from './containers/layers/NotifyLayer';
-
-// TODO: remove testLayer for prod
-import TestLayer from './containers/layers/TestLayer';
-import { useCachedState } from './hooks';
-import YieldNav from './components/YieldNav';
-
-
 const App = (props:any) => {
-  
-  const { state: { seriesLoading, activeSeries } } = useContext(SeriesContext);
-  const { state: { yieldLoading } } = useContext(YieldContext);
-
-  const [cachedLastVisit, setCachedLastVisit] = useCachedState('lastVisit', null);
-
-  const location = useLocation();
-  React.useEffect(() => {
-    location?.pathname !== '/borrow/collateral/' &&
-    ['borrow', 'lend', 'pool'].includes(location.pathname.split('/')[1]) &&
-    setCachedLastVisit(`/${location.pathname.split('/')[1]}/${activeSeries?.maturity}` );
-  }, [location]);
-
-  const [showConnectLayer, setShowConnectLayer] = useState<string|null>(null);
-  const leftSideRef = useRef<any>(null);
-  
-  // TODO remove for prod
-  const [showTestLayer, setShowTestLayer] = useState<boolean>(false);
-
   const screenSize = useContext(ResponsiveContext);
-  const [columns, setColumns] = useState<string[]>(['10%', 'auto', '10%']);
-  useEffect(()=> {
-    screenSize === 'small'?
-      setColumns(['0%', 'auto', '0%'])
-      : setColumns(['10%', 'auto', '10%']);
-  }, [screenSize]);
-
   return (
     <div 
       className="App" 
-      style={
-        ( activeSeries && props.moodLight && screenSize!=='small') ? 
-          { background: `radial-gradient(at 90% 90%, transparent 75%, ${modColor(activeSeries.seriesColor, 50)})` }
-          :
-          undefined
-      }
+      style={{ background:'black' }}
     >
-      <ConnectLayer view={showConnectLayer} closeLayer={() => setShowConnectLayer(null)} />
-      { showTestLayer  && <TestLayer closeLayer={()=>setShowTestLayer(false)} /> }
-
-      <Grid 
-        fill 
-        columns={columns} 
-        justify='center'
-      >
-        <Box background={{ color: 'background-front' }} />
-        <YieldHeader
-          openConnectLayer={(v:string) => setShowConnectLayer(v)}
-        />
-        <Box background={{ color: 'background-front' }} />
-      </Grid>
-
-      { !yieldLoading &&
-        <Collapsible open={!seriesLoading} ref={leftSideRef}>
-          <Authorization />
-        </Collapsible>}
-
-      <NotifyLayer target={leftSideRef.current} columnsWidth={columns} />
-
-      { screenSize !== 'small' &&
-      <Box margin={{ top:'large' }} align='center'>
-        <YieldNav />
-      </Box>}
-
       <Main 
-        pad={{ bottom:'large' }}
-      >      
-        <Box
-          pad={{ vertical: 'large' }}
-          align='center'
-        > 
-          <Switch>
-            <Route path="/borrow/collateral/:amnt?">
-              <Deposit openConnectLayer={() => setShowConnectLayer('CONNECT')} />
-            </Route>
-            <Route path="/borrow/:series?/:amnt?">
-              <Borrow openConnectLayer={() => setShowConnectLayer('CONNECT')} />
-            </Route>
-            <Route path="/lend/:series?/:amnt?">
-              <Lend openConnectLayer={() => setShowConnectLayer('CONNECT')} />
-            </Route>
-            <Route path="/pool/:series?/:amnt?">
-              <Pool openConnectLayer={() => setShowConnectLayer('CONNECT')} />
-            </Route>
-            <Route exact path="/">
-              <Redirect to={`${cachedLastVisit || '/borrow/'}`} />
-            </Route>
-            <Route path="/*">
-              404
-            </Route>
-          </Switch>
-        </Box>               
-      </Main>
-
-      <Grid 
-        fill 
-        columns={columns} 
+        align='center'
         justify='center'
-      >
-        <Box />
-        {screenSize !== 'small' &&
-          <YieldFooter
-            showTestLayer={showTestLayer}
-            setShowTestLayer={setShowTestLayer}
-            darkMode={props.darkMode}
-            setDarkMode={props.setDarkMode}
-            moodLight={props.moodLight}
-            toggleMoodLight={props.toggleMoodLight}
-            openConnectLayer={() => setShowConnectLayer('CONNECT')}
-          />}                  
-        <Box />
-      </Grid>
-
-      {screenSize === 'small' &&    
-        <Layer
-          position='bottom'
-          modal={false}
-          responsive={false}
-          full='horizontal'
+      >  
+        <Box 
+          fill 
+          width={screenSize==='small'? '20%':undefined} 
+          pad='xxsmall'
+          align='center'
+          justify='center'
         >
-          {/* <MobileButton /> */}
-          <Nav
-            direction="row"
-            background="background-mid"          
-            round={{ corner:'top', size:'small' }}
-            elevation='small'
-            pad="medium"
-            justify='evenly'
-          >
-            <Box><YieldMark /></Box>
-            <Box>Collateral</Box>
-            <Box>Borrow</Box>
-            <Box>Repay</Box>         
-          </Nav>
-        </Layer>}
+          <Image src={logoLight} fit="contain" />
+        </Box>
+        <Text size='xxsmall' color='text-xweak'> coming soon...</Text>    
+      </Main>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
-
-import { ethers } from 'ethers';
-import { Web3ReactProvider, createWeb3ReactRoot } from '@web3-react/core';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { NotifyProvider }  from './contexts/NotifyContext';
-import { YieldProvider }  from './contexts/YieldContext';
-import { UserProvider }  from './contexts/UserContext';
-import { SeriesProvider }  from './contexts/SeriesContext';
-
-function getLibrary(provider: any) {
-  const library = new ethers.providers.Web3Provider(provider);
-  library.pollingInterval = 12000;
-  return library;
-}
-function getFallbackLibrary(provider: any) {
-  const library = new ethers.providers.JsonRpcProvider(provider);
-  // const library = ethers.getDefaultProvider('ropsten');
-  library.pollingInterval = 12000;
-  return library;
-}
-let updateAvailable;
-const Web3ReactProviderFallback = createWeb3ReactRoot('fallback');
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Web3ReactProvider getLibrary={getLibrary}> 
-        <Web3ReactProviderFallback getLibrary={getFallbackLibrary}> 
-          <NotifyProvider updateAvialable={updateAvailable}>
-            <YieldProvider>
-              <UserProvider>
-                <SeriesProvider>
-                  <App />
-                </SeriesProvider>
-              </UserProvider>
-            </YieldProvider>
-          </NotifyProvider>
-        </Web3ReactProviderFallback>
-      </Web3ReactProvider>
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -52,7 +16,6 @@ ReactDOM.render(
 serviceWorker.register({ 
   // TODO handle the update
   onUpdate: ()=> {
-    updateAvailable=true;
     console.log( 'A newer version of the app is available' );
-  } 
+  }
 }); 
