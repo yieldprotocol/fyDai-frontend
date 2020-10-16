825da7193d78404eabdc0a577d55a416eb5b479c
Bruce Donovan <brucedonovan@Bruces-MacBook-Pro.local>
2020-10-16 12:28:38 +0100

    remove excess netwrok ids

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -5,10 +5,7 @@ const POLLING_INTERVAL = 12000;
 
 const RPC_URLS: { [chainId: number]: string } = {
   1: process.env.REACT_APP_RPC_URL_1 as string,
-  4: process.env.REACT_APP_RPC_URL_4 as string,
-  5: process.env.REACT_APP_RPC_URL_5 as string,
   42: process.env.REACT_APP_RPC_URL_42 as string,
-  1337: process.env.REACT_APP_RPC_URL_1337 as string,
   31337: process.env.REACT_APP_RPC_URL_31337 as string,
 };
 
