680af03dd4ae662bc652f41ea18ebd9ea8aeccf5
brucedonovan <bruce.donovan@gmail.com>
2021-02-01 11:16:41 +0000

    test update

--- src/utils/math.test.ts
+++ src/utils/math.test.ts
@@ -1,11 +1,8 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { BigNumber } from 'ethers';
-
 import * as withEthers from './yieldMath';
-import * as withMath from './math_mathjs';
 
-const ZERO: BigNumber = BigNumber.from('0');
-const ONE: BigNumber  = BigNumber.from('1');
-const TWO: BigNumber  = BigNumber.from('2');
+const { bignumber, add, subtract, multiply, divide, pow, floor }  = require('mathjs');
 
 const daiReserves = 1450000;
 const fyDaiReserves = 1023400;
@@ -17,6 +14,158 @@ const timeTillMaturity = 200000;
 const fyDaiRealReserves = 3400;
 const fyDaiVirtualReserves = 2023400;
 
+// https://www.desmos.com/calculator/mllhtohxfx
+export function mint(daiRes: any, fyDaiRes: any, sup: any, d: any): [any, any] {
+  const Z = bignumber(daiRes);
+  const Y = bignumber(fyDaiRes);
+  const S = bignumber(sup);
+  const z = bignumber(d);
+  const m = divide(multiply(S, z), Z);
+  const y = divide(multiply(Y, m), S);
+
+  return [m, y];
+}
+
+// https://www.desmos.com/calculator/ubsalzunpo
+export function burn(daiRes: any, fyDaiRes: any, sup: any, lpT: any): [any, any] {
+  const Z = bignumber(daiRes);
+  const Y = bignumber(fyDaiRes);
+  const S = bignumber(sup);
+  const x = bignumber(lpT);
+  const z = divide(multiply(x, Z), S);
+  const y = divide(multiply(x, Y), S);
+
+  return [z, y];
+}
+
+// https://www.desmos.com/calculator/5nf2xuy6yb
+export function sellDai(daiRes: any, fyDaiRes: any, d: any, ttm: any): any {
+  const fee = bignumber(1000000000000);
+  const Z = bignumber(daiRes);
+  const Y = bignumber(fyDaiRes);
+  const T = bignumber(ttm);
+  const x = bignumber(d);
+  const k = bignumber(1 / (4 * 365 * 24 * 60 * 60)); // 1 / seconds in four years
+  const g = bignumber(950 / 1000);
+  const t = multiply(k, T);
+  const a = subtract(1, multiply(g, t));
+  const invA = divide(1, a);
+  const Za = pow(Z, a);
+  const Ya = pow(Y, a);
+  const Zxa = pow(add(Z, x), a);
+  const sum = subtract(add(Za, Ya), Zxa);
+  const y = subtract(Y, pow(sum, invA));
+  const yFee = subtract(y, fee);
+
+  return y.toString();
+}
+
+// https://www.desmos.com/calculator/6jlrre7ybt
+export function sellFYDai(daiRes: any, fyDaiRes: any, fyD: any, ttm: any): any {
+  const fee = bignumber(1000000000000);
+  const Z = bignumber(daiRes);
+  const Y = bignumber(fyDaiRes);
+  const T = bignumber(ttm);
+  const x = bignumber(fyD);
+  const k = bignumber(1 / (4 * 365 * 24 * 60 * 60)); // 1 / seconds in four years
+  const g = bignumber(1000 / 950);
+  const t = multiply(k, T);
+  const a = subtract(1, multiply(g, t));
+  const invA = divide(1, a);
+  const Za = pow(Z, a);
+  const Ya = pow(Y, a);
+  const Yxa = pow(add(Y, x), a);
+  const sum = add(Za, subtract(Ya, Yxa));
+  const y = subtract(Z, pow(sum, invA));
+  const yFee = subtract(y, fee);
+
+  return y.toString();
+}
+
+// https://www.desmos.com/calculator/0rgnmtckvy
+export function buyDai(daiRes: any, fyDaiRes: any, d: any, ttm: any): any {
+  const fee = bignumber(1000000000000);
+  const Z = bignumber(daiRes);
+  const Y = bignumber(fyDaiRes);
+  const T = bignumber(ttm);
+  const x = bignumber(d);
+  const k = bignumber(1 / (4 * 365 * 24 * 60 * 60)); // 1 / seconds in four years
+  const g = bignumber(1000 / 950);
+  const t = multiply(k, T);
+  const a = subtract(1, multiply(g, t));
+  const invA = divide(1, a);
+  const Za = pow(Z, a);
+  const Ya = pow(Y, a);
+  const Zxa = pow(subtract(Z, x), a);
+  const sum = subtract(add(Za, Ya), Zxa);
+  const y = subtract(pow(sum, invA), Y);
+  const yFee = add(y, fee);
+
+  return y.toString();
+}
+
+// https://www.desmos.com/calculator/ws5oqj8x5i
+export function buyFYDai(daiRes: any, fyDaiRes: any, fyD: any, ttm: any): any {
+  const fee = bignumber(1000000000000);
+  const Z = bignumber(daiRes);
+  const Y = bignumber(fyDaiRes);
+  const T = bignumber(ttm);
+  const x = bignumber(fyD);
+  const k = bignumber(1 / (4 * 365 * 24 * 60 * 60)); // 1 / seconds in four years
+  const g = bignumber(950 / 1000);
+  const t = multiply(k, T);
+  const a = subtract(1, multiply(g, t));
+  const invA = divide(1, a);
+  const Za = pow(Z, a);
+  const Ya = pow(Y, a);
+  const Yxa = pow(subtract(Y, x), a);
+  const sum = add(Za, subtract(Ya, Yxa));
+  const y = subtract(pow(sum, invA), Z);
+  const yFee = add(y, fee);
+
+  return y.toString();
+}
+
+export function fyDaiForMint(
+  daiRes: any,
+  fyDaiRealRes: any,
+  fyDaiVirtualRes: any,
+  d: any,
+  ttm: any
+): any {
+  const Z = bignumber(daiRes);
+  const YR = bignumber(fyDaiRealRes);
+  const YV = bignumber(fyDaiVirtualRes);
+  const z = bignumber(d);
+  const t = bignumber(ttm);
+
+  let min = bignumber(0);
+  let max = z;
+  let y_out = divide(add(min, max), bignumber(2)); // average
+
+  let i = 0;
+  while (true) {
+    const z_in = bignumber(buyFYDai(Z, YV, y_out, t));
+    const Z_1 = add(Z, z_in); // New dai reserves
+    const Y_1 = subtract(YR, y_out); // New fyDai reserves
+
+    const pz = divide(subtract(z, z_in), add(subtract(z, z_in), y_out)); // dai proportion in my assets
+    const PZ = divide(Z_1, add(Z_1, Y_1)); // dai proportion in the reserves
+
+    // The dai proportion in my assets needs to be higher than but very close to the dai proportion in the reserves, to make sure all the fyDai is used.
+    if (multiply(PZ, bignumber(1.000001)) <= pz) min = y_out;
+    y_out = divide(add(y_out, max), bignumber(2)); // bought too little fyDai, buy some more
+    if (pz <= PZ) max = y_out;
+    y_out = divide(add(y_out, min), bignumber(2)); // bought too much fyDai, buy a bit less
+    // console.log(`y = ${floor(y_out).toFixed()}\n`)
+
+    if (multiply(PZ, bignumber(1.000001)) > pz && pz > PZ) return floor(y_out).toFixed(); // Just right
+
+    // eslint-disable-next-line no-plusplus
+    if (i++ > 10000) return floor(y_out).toFixed();
+  }
+}
+
 it('Should give the same result for mint()', () => {
   expect( 
     withEthers.mint(
@@ -26,7 +175,7 @@ it('Should give the same result for mint()', () => {
       BigNumber.from(dai) 
     ).map( (x: BigNumber) => x.toString() )
   ).toEqual(
-    withMath.mint(daiReserves, fyDaiReserves, supply, dai).map( (x:any) =>  x.toString())
+    mint(daiReserves, fyDaiReserves, supply, dai).map( (x:any) =>  x.toString())
   );
 });
 
@@ -39,7 +188,7 @@ it('Should give the same result for burn()', () => {
       BigNumber.from(lpTokens)
     ).map( (x: BigNumber) => x.toString() )
   ).toEqual(
-    withMath.burn(daiReserves, fyDaiReserves, supply, lpTokens).map( (x:any) => x.toString())
+    burn(daiReserves, fyDaiReserves, supply, lpTokens).map( (x:any) => x.toString())
   );
 });
 
@@ -52,7 +201,7 @@ it('Should give the same result for sellDai()', () => {
       BigNumber.from(timeTillMaturity)
     ).toString()
   ).toEqual(
-    withMath.sellDai(daiReserves, fyDaiReserves, dai, timeTillMaturity).toString()
+    sellDai(daiReserves, fyDaiReserves, dai, timeTillMaturity).toString()
   );
 });
 
@@ -65,7 +214,7 @@ it('Should give the same result for sellFYDai()', () => {
       BigNumber.from(timeTillMaturity)
     ).toString()
   ).toEqual(
-    withMath.sellFYDai(daiReserves, fyDaiReserves, fyDai, timeTillMaturity).toString()
+    sellFYDai(daiReserves, fyDaiReserves, fyDai, timeTillMaturity).toString()
   );
 });
 
@@ -78,7 +227,7 @@ it('Should give the same result for buyDai', () => {
       BigNumber.from(timeTillMaturity)
     ).toString()
   ).toEqual(
-    withMath.buyDai(daiReserves, fyDaiReserves, fyDai, timeTillMaturity).toString()
+    buyDai(daiReserves, fyDaiReserves, fyDai, timeTillMaturity).toString()
   );
 });
 
@@ -91,7 +240,7 @@ it('Should give the same result for buyFYDai', () => {
       BigNumber.from(timeTillMaturity)
     ).toString()
   ).toEqual(
-    withMath.buyFYDai(daiReserves, fyDaiReserves, dai, timeTillMaturity).toString()
+    buyFYDai(daiReserves, fyDaiReserves, dai, timeTillMaturity).toString()
   );
 });
 
@@ -105,24 +254,6 @@ it('Should give the same result for fyDaiForMint()', () => {
       BigNumber.from(timeTillMaturity)
     ).toString()
   ).toEqual(
-    withMath.fyDaiForMint(daiReserves, fyDaiRealReserves, fyDaiVirtualReserves, dai, timeTillMaturity).toString()
-  );
-});
-
-it('Should calculate', () => {
-  expect(
-    withEthers.mint(
-      BigNumber.from(daiReserves),
-      BigNumber.from(fyDaiReserves),
-      BigNumber.from(supply), 
-      BigNumber.from(dai),
-    ).toString()
-  ).toEqual(
-    withEthers.calcTokensMinted(
-      BigNumber.from(daiReserves),
-      BigNumber.from(fyDaiReserves),
-      BigNumber.from(supply), 
-      BigNumber.from(dai),
-    ).toString()
+    fyDaiForMint(daiReserves, fyDaiRealReserves, fyDaiVirtualReserves, dai, timeTillMaturity).toString()
   );
 });
