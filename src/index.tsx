a478c6c221f03d58104945fef8fb7e48a7726771
brucedonovan <bruce.donovan@gmail.com>
2021-02-18 12:13:12 +0000

    feat: node connection

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
@@ -15,16 +16,28 @@ import { TxProvider } from './contexts/TxContext';
 import { HistoryProvider } from './contexts/HistoryContext';
 
 function getLibrary(provider: any) {
+
   const library = new ethers.providers.Web3Provider(provider);
   library.pollingInterval = 12000;
   return library;
 }
+
 function getFallbackLibrary(provider: any) {
-  const library = new ethers.providers.InfuraProvider(provider.chainId, '646dc0f33d2449878b28e0afa25267f6');
-  // const library = ethers.getDefaultProvider('ropsten');
+  
+  let library: ethers.providers.InfuraProvider | ethers.providers.JsonRpcProvider ;
+  
+  process.env.REACT_APP_FORCE_ENV === 'development' || process.env.NODE_ENV === 'development' ?
+    library = new ethers.providers.JsonRpcProvider() :
+    library = new ethers.providers.InfuraProvider(provider.chainId, '646dc0f33d2449878b28e0afa25267f6');
+
+  process.env.REACT_APP_FORCE_ENV === 'development' || process.env.NODE_ENV === 'development' ? 
+    console.log('Using RPC Provider as fallback') : console.log('Using Infura Provider as fallback');
+
   library.pollingInterval = 12000;
+
   return library;
 }
+
 const Web3ReactProviderFallback = createWeb3ReactRoot('fallback');
 
 ReactDOM.render(
